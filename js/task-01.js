30c8de0b565c93ed377d88b08b7d3d55518f94f2
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-22 00:52:20 +0300

    1. with nav

--- js/task-01.js
+++ js/task-01.js
@@ -4,11 +4,20 @@ console.log(`Number of categories: `, listWithLiItemAll.length);
 // console.log(listWithLiItemAll);
 
 listWithLiItemAll.forEach((item) => {
-  const listWithH2 = item.querySelector("h2");
-  const listWithLi = item.querySelectorAll("li");
-  console.log(`Category: `, listWithH2.textContent);
-  console.log(`Elements: `, listWithLi.length);
+  // debugger;
+  console.log(`Category: `, item.firstElementChild.textContent);
+  console.log(`Elements: `, item.lastElementChild.children.length);
 });
+
+
+// console.log(`!!!!!!!!!!!!`);
+
+// listWithLiItemAll.forEach((item) => {
+//   const listWithH2 = item.querySelector("h2");
+//   const listWithLi = item.querySelectorAll("li");
+//   console.log(`Category: `, listWithH2.textContent);
+//   console.log(`Elements: `, listWithLi.length);
+// });
 
 // console.log(`!!!!!!!!!!!!`);
 
