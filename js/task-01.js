8379a933eb6179fbc320a0e83484d3127d83a265
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-21 18:42:53 +0300

    1. foEach, for of

--- js/task-01.js
+++ js/task-01.js
@@ -3,9 +3,22 @@ console.log(`Number of categories: `, listWithLiItemAll.length);
 
 // console.log(listWithLiItemAll);
 
-for (const item of listWithLiItemAll) {
+listWithLiItemAll.forEach((item) => {
   const listWithH2 = item.querySelector("h2");
   const listWithLi = item.querySelectorAll("li");
   console.log(`Category: `, listWithH2.textContent);
+  console.log(listWithH2);
   console.log(`Elements: `, listWithLi.length);
-}
+  console.log(listWithLi);
+});
+
+// console.log(`!!!!!!!!!!!!`);
+
+// for (const item of listWithLiItemAll) {
+//   const listWithH2 = item.querySelector("h2");
+//   const listWithLi = item.querySelectorAll("li");
+//   console.log(`Category: `, listWithH2.textContent);
+//     console.log(listWithH2);
+//   console.log(`Elements: `, listWithLi.length);
+//     console.log(listWithLi);
+// }
