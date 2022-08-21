6bda3c04c33d3022c37d469ebda516475fc91d79
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-22 01:57:17 +0300

    2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,25 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map((item) => {
+  console.log(item);
+
+  const titleEl = document.createElement("li");
+  titleEl.classList.add("item");
+  titleEl.textContent = item;
+
+  console.log(titleEl);
+
+  return titleEl;
+});
+
+console.log(elements);
+ingredientsEl.append(...elements);
