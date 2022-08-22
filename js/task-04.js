ed7f2f1ba95f0dd955778774ee00e1fa87a031dd
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-22 21:08:00 +0300

    4

---
+++ js/task-04.js
@@ -0,0 +1,13 @@
+let counterValue = 0;
+const decrementEl = document.querySelector('button[data-action="decrement"]');
+const incrementEl = document.querySelector('button[data-action="increment"]');
+const valueEl = document.querySelector("#value");
+
+decrementEl.addEventListener("click", () => {
+  counterValue -= 1;
+  valueEl.textContent = counterValue;
+});
+incrementEl.addEventListener("click", () => {
+  counterValue += 1;
+  valueEl.textContent = counterValue;
+});
