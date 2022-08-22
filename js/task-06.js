9478b2b4879b9bb48b7a2bad516f017826c112bf
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-22 23:53:57 +0300

    6.fin

--- js/task-06.js
+++ js/task-06.js
@@ -4,11 +4,11 @@ inputEl.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
   const currentInput = event.currentTarget;
+  const inputLength = Number(currentInput.dataset.length);
 
   currentInput.classList.add("invalid");
 
-
-  if (currentInput.value.length === 6) {
+  if (currentInput.value.length === inputLength) {
     currentInput.classList.remove("invalid");
     currentInput.classList.add("valid");
   }
