4751edfddcdf745810a8c3c3da18c7787691efa7
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-23 20:14:46 +0300

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const BtnEl = document.querySelector(".change-color");
+const colorEl = document.querySelector(".color");
+
+BtnEl.addEventListener("click", changeColor);
+
+function changeColor() {
+  colorEl.textContent = getRandomHexColor();
 }
