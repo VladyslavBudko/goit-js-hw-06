1f1751238301c00232a99c191396d92dd55e7ded
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-23 19:45:44 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,31 @@
+const refs = {
+  email: document.querySelector("email"),
+  password: document.querySelector("password"),
+  button: document.querySelector("submit"),
+  form: document.querySelector(".login-form"),
+};
 
+refs.form.addEventListener("submit", onFormSubmitBtn);
+
+function onFormSubmitBtn(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  //   console.log(formData);
+
+  formData.forEach((value, name) => {
+    if (value || name === " ") {
+      alert("Input data");
+      document.location.reload();
+    }
+
+    console.log("name:", name);
+    console.log("value:", value);
+  });
+
+  //   const {
+  //     elements: { email, password },
+  //   } = event.currentTarget;
+  //   console.log(email.value, password.value);
+  //   console.dir(event.currentTarget);
+}
