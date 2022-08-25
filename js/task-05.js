87de518e65aed86b692a15aa72d2f1b366c478cb
Vladyslav Budko <vladyslav.budko@gmail.com>
2022-08-25 17:20:00 +0300

    5. fixed

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,12 @@ const nameInputEl = document.querySelector("#name-input");
 const nameOutputEl = document.querySelector("#name-output");
 
 nameInputEl.addEventListener("input", (event) => {
-  nameOutputEl.textContent = event.currentTarget.value;
+
+  nameOutputEl.textContent = event.currentTarget.value || "Anonymous";
+
+  // console.log(event.currentTarget.value);
+  // if (event.currentTarget.value === '') {
+  //   nameOutputEl.textContent = "Anonymous";
+  // } else nameOutputEl.textContent = event.currentTarget.value;
+
 });
